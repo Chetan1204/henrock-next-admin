ce7d0c90768f3ceca6a51254e1c8ef1de1fa462a
Chetan Kharat <chetankharat0628@gmail.com>
2025-07-03 19:10:01 +0530

    qr edit

--- src/pages/QrCode.tsx
+++ src/pages/QrCode.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import QrCodeComponent from "components/doctors/QrcodeComponent";
+
 
 const QrCodePage: React.FC = () => {
   return (
     <>
-      <QrCodeComponent />
+      
     </>
   );
 };
